d96a813efa1330122fa0907dec639358cd55d41f
AnandaSH-8 <anandsholla8@gmail.com>
2024-10-12 22:34:54 +0530

    home images updated

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Style from "./home.module.css";
+import { Margin } from "@mui/icons-material";
 
 export const Home = () => {
   let images = [
-    "https://m.media-amazon.com/images/S/aplus-media/vc/19f7a119-76b7-4017-a3d4-d17a9e2bba77.__CR0,0,1464,600_PT0_SX1464_V1___.jpg",
-    "https://techygalaxy.com/wp-content/uploads/2021/10/xx-7.jpg",
-    "https://pbs.twimg.com/media/FOwh2JLWUAA3HFa.jpg",
-    "https://www.androidauthority.com/wp-content/uploads/2020/08/redmi-9-india.jpg",
-    "https://cdn.paisawapas.com/blog/wp-content/uploads/2018/07/banner.jpg",
+    "https://realtimeworld.in/wp-content/uploads/2024/07/Best-Phones-Under-10000-in-2024-860x487.jpg",
+    "https://www.brutimes.com/cache/top%2010%20mobile%20phones%20under%2020000_2100_x_1470.webp",
+    "https://www.unboxtheory.com/wp-content/uploads/2024/06/New-Project-2024-06-09T043410.986-1.jpg",
+    "https://www.poco.in/images/poco-M6-Pro-5g/M/page3.png",
+    "https://exstatic-in.iqoo.com/Oz84QB3Wo0uns8j1/in/1724143639774/9b2b7b82d943f8c08a362ab9b0d1edbd.jpg_w860-h860.webp",
   ];
 
   const [image, setImage] = useState();
@@ -30,7 +31,7 @@ export const Home = () => {
         <img src={image} alt="Phones" />
       </div>
       <div className={Style.catergories_head}>
-        <h1>Categories</h1>
+        <h1 style={{paddingTop:'10px'}} >Categories</h1>
         <div className={Style.subTitle}>
           <kbd>₹10000</kbd>
           <kbd>₹20000</kbd>
@@ -38,14 +39,14 @@ export const Home = () => {
         <div className={Style.cat_images}>
           <Link to="/mobiles/under_10000">
             <img
-              src="https://www.pricecheckindia.in/wp-content/uploads/2021/04/phones-1.png"
+              src="https://www.triveniworld.com/cdn/shop/collections/buy-smart-phone-under-10000-triveni-world_616c941e-15c0-4254-ae02-9796170db743.jpg?v=1717480224"
               alt="10000"
               loading="eager"
             />
           </Link>
           <Link to="/mobiles/under_20000">
             <img
-              src="https://www.shoppingum.com/media/blog/banner_3s7nrq2.jpg"
+              src="https://i.ytimg.com/vi/vJhmwdi9eug/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDFBD46HUV71s-OkV3-uGqf4A1RQQ"
               alt=""
             />
           </Link>
